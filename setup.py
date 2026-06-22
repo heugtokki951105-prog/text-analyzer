d43e559875b6da5138629111651d9c8f5588375b
박윤준 <heugtokki951105@gmail.com>
2026-06-22 10:35:55 +0900

    Add package description metadata

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="text_analyzer",
+    version="0.1.0",
+    author="박윤준",
+    description="A simple Python package for text and social media analysis.",
+    packages=find_packages(),
+    install_requires=[],
+)
